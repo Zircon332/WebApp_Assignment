7316d6f45c22bd8452b0be71164be7c3b468f360
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2019-04-26 14:56:56 +0800

    added comments to script.js

--- assign1/script/script.js
+++ assign1/script/script.js
@@ -5,3 +5,56 @@ Author: Ryan Yap, Wong Kah Heng, Joel Yong, Wong Jun Jie
 Date written: 26/4/2019
 Revisions:  [ your name, what, when…]                                           //needs detailes
 */
+
+
+// -----Data Transfer between Web pages----- //
+
+// [ Function description ]
+
+
+
+
+
+
+
+// -----Data transfer in the same page----- //
+
+// [ Function description ]
+
+
+
+
+
+
+
+// -----Data validation and input checking of Forms-----  //
+
+
+// [ Function description ]
+
+
+
+
+
+
+// -----Populate drop-down list using Javascript----- //
+
+// [ Function description ]
+
+
+
+
+
+
+
+// -----init----- //
+function init () {
+  // test if javascript is in pages
+  alert("Javascript active");
+
+  // call data transfer function
+
+}
+
+
+window.onload = init;
