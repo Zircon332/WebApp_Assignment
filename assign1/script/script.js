1fa22290d4484df2727c45b9a79482113cace33d
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2019-04-26 14:48:17 +0800

    added script.js, added script link to all html

---
+++ assign1/script/script.js
@@ -0,0 +1,7 @@
+/* Filename: script.js
+Target html: [ what is the html file(s) linked to this js…]                     //needs detailes
+Purpose : [ a html file may have multiple js files. What does this one do?…]    //needs detailes
+Author: Ryan Yap, Wong Kah Heng, Joel Yong, Wong Jun Jie
+Date written: 26/4/2019
+Revisions:  [ your name, what, when…]                                           //needs detailes
+*/
