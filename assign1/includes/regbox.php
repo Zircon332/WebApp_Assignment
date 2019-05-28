13aa9adebeb82d9ec47b221ab1070ba6e41666d4
Zircon332 <43201658+Zircon332@users.noreply.github.com>
2019-05-28 15:50:58 +0800

    Added regbox.php and css style for it

---
+++ assign1/includes/regbox.php
@@ -0,0 +1,5 @@
+<aside id="regbox">
+    <h1> Register today and become a member!</h1>
+    <h2>Get news and latest information from your favourite rental store!</h2>
+    <button class="button" onclick="window.location.href='enquiry.html'"><span>REGISTER HERE</span></button>
+</aside>
